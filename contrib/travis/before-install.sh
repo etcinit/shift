64b2cbda19e7f4e6ded9749fafac572c203c8e2e
Eduardo Trujillo <ed@chromabits.com>
2016-09-17 21:07:51 -0700

    fix(travis): Change where `stack docker pull` happens

--- contrib/travis/before-install.sh
+++ contrib/travis/before-install.sh
@@ -9,7 +9,7 @@ export PATH=$HOME/.local/bin:$PATH
 curl -L https://www.stackage.org/stack/linux-x86_64 \
   | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 
-stack setup
+stack docker pull
 stack install hscolour
 
 docker login -e="." -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
